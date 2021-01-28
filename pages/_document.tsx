756a5686fcb62df5e4ed2bd31898834ecd67f997
wwsalmon <wwsamson@yahoo.com>
2021-01-28 16:05:22 -0700

    basic setup

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return initialProps;
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <title>Next Tailwind Typescript Example</title>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
